c61ad540c5565c8e1271ce3756e923d00f83714e
Matthew Swartwout <mwswartwout@gmail.com>
2015-09-22 10:48:37 -0400

    Initial commit

---
+++ src/minimal_commander.cpp
@@ -0,0 +1,26 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+#include <math.h>
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "minimal_commander"); // minimal_commander node
+    ros::NodeHandle n;
+    ros::Publisher command_publisher = n.advertise<std_msgs::Float64>("vel_cmd", 1); // publish to vel_cmd topic
+    ros::Rate naptime(10); // update @ 10hz
+
+    double amplitude = 1; // amplitude value for sine wave
+    double frequency = 4; // frequency value for sine wave
+    double pi = 3.14159; // value of pi
+    double t = 0; // current time in calculation
+    double dt = 0.1; // timestep for calculation
+    double sine; // sine output
+    std_msgs::Float64 output; // message wrapper for sine output
+
+    while (ros::ok()) {
+        sine = amplitude * sin(2*pi*frequency*t); // Calculate sine value
+        output.data = sine; // Store sine value in proper message format
+        command_publisher.publish(output); // Publish value to vel_cmd topic
+        t += dt; // Increment t by timeset dt
+        naptime.sleep();
+    }
+}
